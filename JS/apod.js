65ce11dbe012164d4f58753719a627abf384a36d
Rafalesson <rafaalesson35@gmail.com>
2022-02-12 01:32:44 -0300

    Agora foi, papay!

--- JS/apod.js
+++ JS/apod.js
@@ -1,27 +1,52 @@
 let form = $('#form')
 let data = $('#data')
 
-form.submit(function (evento){
+form.submit(function (evento) {
     evento.preventDefault()
-    
+
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=qxrLRupjtxkUzvA0Pdyz1tWck3Q0ehEQaWy6fXZt&date=${data.val()}`,
-    
-        success: function (dado){
-            $('#resultado').css({display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly', background: 'rgba(92, 112, 139, 0.4)', })
-            $('.pesquisa').css('justify-content', 'space-evenly')
-            console.log(dado)
-            $('#resultado').html(`
-            <h1 class="titulo-resultado">${dado.title}</h1>
+
+        success: function (dado) {
+            $('#resultado').css({
+                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly', background: 'rgba(255, 255, 255, 0.2)',
+            })
+
+            $('#resultado').html(`<h1 class="titulo-resultado">
+            ${dado.title}</h1>
             <p class="texto-resultado">${dado.explanation}</p>
-            <img class="img-resultado" src="${dado.url}"/> 
-            `)
+            <img class="img-resultado" src="${dado.url}"/>`)
+
+            $('.titulo-resultado').css({
+                fontSize: '1.5em',
+                textShadow: '0px 0px 10px rgba(0, 73, 175, .3)'
+            })
+
+            $('.texto-resultado').css({
+                width: '80%',
+                height: 'auto',
+                fontSize: '1em',
+                textAlign: 'justify',
+                background: '#dadada',
+                padding: '15px',
+                borderRadius: '15px',
+                boxShadow: '0px 0px 10px rgb(0, 73, 175)',
+
+            })
+
+            $('.img-resultado').css({
+                width: '90%',
+                height: '300px',
+                borderRadius: '15px',
+                boxShadow: '0px 0px 10px rgb(0, 73, 175)',
+
+            })           
         },
-    
-        error: function(erro){
+
+        error: function (erro) {
             console.log(erro)
         }
-        
+
     })
 })
 
